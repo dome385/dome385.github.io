d259a06a4a7a34cc538f6cedf633ee03412fce40
dome385 <dominik.niestroj@gmail.com>
2024-04-24 13:07:09 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 
-      (function (d, t) {
-        var v = d.createElement(t),
-          s = d.getElementsByTagName(t)[0];
-        v.onload = function () {
-          window.voiceflow.chat
-            .load({
-              verify: { projectID: "6605e1d0e773c6d866e27b2d" },
-              url: "https://general-runtime.voiceflow.com",
-              versionID: "production",
-            })
-            .then(() => {
-              window.voiceflow.chat.proactive.push({
-                type: "text",
-                payload: {
-                  message:
-                    "Starten Sie die Revolution Ihres Unternehmens – klicken Sie hier, um mit unserem Agenten zu sprechen!",
-                },
-              });
-            });
-        };
-        v.src = "https://cdn.jsdelivr.net/gh/dome385/bundle/bundle.mjs";
-        v.type = "text/javascript";
-        s.parentNode.insertBefore(v, s);
-      })(document, "script");
+       (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '6628ac3e03717fc094131ecc' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          mode: 'embedded',
+          target: document.getElementById('root'),
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+    })(document, 'script');
